e5fcbe5f0c35618246f5212f7f73dbacd3991bc4
Aleko Neshev <alekonesh@gmail.com>
2023-11-29 11:26:34 +0200

    Like and dislike functionality added

--- ZestFrontend/ViewModels/PostDetailsViewModel.cs
+++ ZestFrontend/ViewModels/PostDetailsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.AspNetCore.SignalR.Client;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -17,26 +18,37 @@ namespace ZestFrontend.ViewModels
          AuthService authService;
          PostsService postsService;
          LikesService likesService;
+         HubConnection connection;
 
         public PostDetailsViewModel(AuthService authService, PostsService postsService, LikesService likesService) 
         { 
             this.authService = authService;
             this.postsService = postsService;
             this.likesService = likesService;
+            connection = new HubConnectionBuilder().WithUrl("https://localhost:7183/likeshub").Build();
+            connection.On<int>("SignalLike", (id) => UpdatePost(id));
+            connection.StartAsync();
         }
         [ObservableProperty]
         PostDTO post;
         public ObservableCollection<CommentDTO> Comments { get; } = new();
 
-        [RelayCommand]
-        async Task DislikePostAsync(PostDTO postDTO)
+        public async void UpdatePost(int id)
         {
-            await likesService.Like(authService.Id, postDTO.Id, 0, false);
+            var updatedPost = await postsService.GetSinglePost(id);
+           /* Post.Likes = updatedPost.Likes;
+            Post.Dislikes = updatedPost.Dislikes;*/
+        }
+
+        [RelayCommand]
+        async Task DislikePostAsync()
+        {
+            await likesService.Like(authService.Id, Post.Id, 0, false);
         }
         [RelayCommand]
-        async Task LikePostAsync(PostDTO postDTO)
+        async Task LikePostAsync()
         {
-            await likesService.Like(authService.Id, postDTO.Id, 0, true);
+            await likesService.Like(authService.Id, Post.Id, 0, true);
         }
         [RelayCommand]
         async Task SendAsync()
