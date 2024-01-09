80de70bc6d5cb49769bc5d8d475996c37675c2ff
Aleko Neshev <alekonesh@gmail.com>
2024-01-09 13:09:14 +0200

    Friends list page added

---
+++ ZestFrontend/DTOs/FollowerDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZestFrontend.DTOs
+{
+	public class FollowerDTO
+	{
+        public int FollowerId { get; set; }
+        public string FollowerUsername { get; set; }
+    }
+}
