a21b8469ae5e989088e2ffb2f389c754326f8bf1
Aleko Neshev <80381400+AlekoNeshev@users.noreply.github.com>
2024-01-17 15:00:36 +0200

    Media can be fetched from api

---
+++ ZestFrontend/Services/MediaService.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZestFrontend.Services
+{
+    public class MediaService
+    {
+        HttpClient _httpClient;
+
+        public MediaService(HttpClient httpClient)
+        {
+            this._httpClient = httpClient;
+        }
+		public async Task<byte[]> GetMedia(string name)
+		{
+			var url = $"https://localhost:7183/api/PostRescources/ivan/{name}";
+			var response = await _httpClient.GetAsync(url);
+			if (response.IsSuccessStatusCode)
+				return await response.Content.ReadAsByteArrayAsync();
+			else
+				return null;
+		}
+		public async Task<HttpResponseMessage> UploadImage(int postId, FileResult postedFile)
+		{
+			var request = $"https://localhost:7183/api/PostRescources/ivan/{postId}";
+			var content = new MultipartFormDataContent();
+			content.Add(new StreamContent(await postedFile.OpenReadAsync()), "postedFile", postedFile.FileName);
+
+			
+			var response = await _httpClient.PostAsync(request, content);
+
+			
+			if (response.IsSuccessStatusCode)
+			{
+				
+				Console.WriteLine("Upload successful");
+			}
+			else
+			{
+				
+				Console.WriteLine("Error uploading file: " + response.StatusCode);
+			}
+			return response;
+		}
+		
+	}
+}
