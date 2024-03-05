0c0af6a1befda85b710782e6ebe1549ad9c2eaee
Aleko Neshev <80381400+AlekoNeshev@users.noreply.github.com>
2024-03-06 00:55:27 +0200

    Buttons fixed - icons added instead of buttons with text

--- ZestFrontend/DTOs/PostResourcesDTO.cs
+++ ZestFrontend/DTOs/PostResourcesDTO.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ZestFrontend.DTOs
 {
-    public class PostResourcesDTO
+    public class PostResourcesDTO : INotifyPropertyChanged
     {
 		public int Id { get; set; }
 		public string Type { get; set; }
-		public string Source { get; set; }
+		private string _source;
+		public string Source
+		{
+			get => _source;
+			set
+			{
+				_source = value;
+				OnPropertyChanged(nameof(Source));
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
 	}
 }
